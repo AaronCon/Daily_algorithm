6593fb0098225bacb31f4ca5b35b4cbbeb11f986
chenjiawen <8928027+chenjiawen666@user.noreply.gitee.com>
2022-10-12 00:12:13 +0800

    删除链表的倒数第n个节点

--- Topic/19.删除链表的倒数第-n-个结点.js
+++ Topic/19.删除链表的倒数第-n-个结点.js
@@ -18,7 +18,20 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-
+  let s = 0
+  let current = new ListNode(null)
+  current.next = head
+  let arr1 = current
+  let arr2 = current
+  do{
+    arr1 = arr1.next
+    s++
+  } while(arr1)
+  for(let i = 0; i < s - n - 1; i++) {
+    arr2 = arr2.next
+  }
+  if(arr2.next) arr2.next = arr2.next.next
+  return current.next
 };
 // @lc code=end
 
