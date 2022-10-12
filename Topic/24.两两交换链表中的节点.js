775c87477c71f5c8562b9dac61d50e90c1f29bd1
chenjiawen <8928027+chenjiawen666@user.noreply.gitee.com>
2022-10-12 23:32:26 +0800

    两两交换链表中的节点

---
+++ Topic/24.两两交换链表中的节点.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+/**
+ * 方法一: 理由虚拟头节点，以两个为单位交换链表
+ */
+var swapPairs = function(head) {
+  if(head === null || head.next === null) return head
+  let prehead = new ListNode(-1), p = prehead
+  p.next = head
+  do {
+    let current = p.next.next
+    p.next.next = p.next.next.next
+    current.next = p.next
+    p.next = current
+    p = p.next.next
+  }while(p.next !== null && p.next.next !== null)
+  return prehead.next
+};
+// @lc code=end
+
