66b32b00a73b55d307e74f181e0f3efbc3f7e21e
AaronCon <2905529185@qq.com>
2022-10-15 19:30:00 +0800

    移除链表元素(递归)

--- Topic/203.移除链表元素.js
+++ Topic/203.移除链表元素.js
@@ -17,19 +17,31 @@
  * @param {number} val
  * @return {ListNode}
  */
+/**
+ * 迭代
+ */
+// var removeElements = function(head, val) {
+//   if(head == null) return head
+//   let prehead = new ListNode(-1)
+//   prehead.next = head
+//   let p = prehead
+//   while(p.next) {
+//     if(p.next.val == val) {
+//       p.next = p.next.next 
+//     } else {
+//       p = p.next
+//     }
+//   }
+//   return prehead.next
+// };
+
+/**
+ * 递归
+ */
 var removeElements = function(head, val) {
-  if(head == null) return head
-  let prehead = new ListNode(-1)
-  prehead.next = head
-  let p = prehead
-  while(p.next) {
-    if(p.next.val == val) {
-      p.next = p.next.next 
-    } else {
-      p = p.next
-    }
-  }
-  return prehead.next
-};
+  if(head === null) return head
+  head.next = removeElements(head.next, val)
+  return head.val === val ? head.next : head
+}
 // @lc code=end
 
